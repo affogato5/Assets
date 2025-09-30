78b4ac7a04bb2735858a00f866ff07d4cd70ceed
Nico-olalia <olalinic000@students.ohsd.net>
2025-09-30 07:30:15 -0700

    killed redundant files

--- BoolScript.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class BoolScript : ElectronicBase
-{
-    // Start is called before the first frame update
-    public bool Bool = false;
-}
--- Electronics/Thruster.cs
+++ Electronics/Thruster.cs
@@ -2,7 +2,6 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.MPE;
 using UnityEngine;
 
 public class Thruster : ElectronicBase
